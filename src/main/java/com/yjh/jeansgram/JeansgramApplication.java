3f47d0002368d574cf52ca0fa893fbda4b54aa28
JIZIM\ckfwl <d>
2023-02-08 04:02:46 +0900

    [허예진] - Spring Project Jeansgram 최초파일 추가

---
+++ src/main/java/com/yjh/jeansgram/JeansgramApplication.java
@@ -0,0 +1,13 @@
+package com.yjh.jeansgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeansgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeansgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yjh/jeansgram/JeansgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjh.jeansgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeansgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
